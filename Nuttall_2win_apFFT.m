75e8c0e6d07a3d82b695bfa63ad1ca962b7ba88f
ThalerG <thalergabriel@gmail.com>
2024-07-11 14:30:04 -0300

    Finished corrected apFFT code

--- Nuttall_2win_apFFT.m
+++ Nuttall_2win_apFFT.m
@@ -1,4 +1,4 @@
-function [y] = Nuttall_2win_apFFT(signal)
+function [f_correct,y_correct] = Nuttall_2win_apFFT(signal, Fs)
 %apFFT Performs the Nuttall double-window all Phase FFT transformation on a 1D array.
 %   This function takes a 1D array 'signal' as input and returns its
 %   all-phase FFT transformation 'y'. The all-phase FFT aims to minimize
@@ -8,25 +8,64 @@ if mod(length(signal),2)==0
     error('Signal cannot be of even length');
 end
 
+plot_graphs = true;
+
 N = (length(signal)+1)/2;
 
-% Step 1: Nuttall window and N-vector grouping
-N_win = nuttall(length(signal),'numTerms',4,'Order',3);
-x_win = signal.*N_win;
+% Nuttall window
+N_win = nuttall(N,'numTerms',4,'Order',3);
+N_win = N_win/sum(N_win);
 
-X_group = NaN([N,N]);
+% Convolution of windows (double windowing)
+N_win_conv = conv(N_win,N_win);
+N_win_conv = N_win_conv/sum(N_win_conv); % Ensures that the sum of the 
+                                         % window is 1
+% Single windowing of last N samples for FFT
+x_win = signal(end-N+1:end).*N_win;
+S_FFT = fft(x_win);
 
-for j = 1:N
-    X_group(:,j) = x_win((N-j+1):(end-j+1));
+% Double-windowing of last 2N-1 samples for apFFT
+x_conv = ((signal-mean(signal)).*N_win_conv);
+S = [x_conv(N), x_conv(1:N-1) + x_conv(N+1:end)];
+
+S_apFFT = fft(S);
+
+k = 1:floor(N/2); % Index for single-sided FFT/apFFT
+
+ % Amplitude of single-sided FFT/apFFT is doubled for non-zero frequencies
+
+S_apFFT = [S_apFFT(1), 2*S_apFFT(k(2:end))];
+S_FFT = [S_FFT(1), 2*S_FFT(k(2:end))];
+
+% Frequency and amplitude correction
+delta_ph = angle(S_FFT(k))-angle(S_apFFT(k));
+f_correct = (delta_ph*N/(pi*(N-1))+k-1)*Fs/N;
+A = abs(S_FFT(k)).^2./abs(S_apFFT(k));
+
+% Complex representation
+ph = angle(S_apFFT(k));
+y_correct = A.*exp(1i.*ph);
+
+if plot_graphs
+    f_FFT = Fs/N*(0:(N/2));
+    
+    figure;
+    ax(1) = subplot(3,1,1);
+    plot(f_FFT(1:end-1),abs(S_FFT(k)),'LineWidth',1)
+    hold on
+    plot(f_FFT(1:end-1),abs(S_apFFT(k)),'LineWidth',1)
+    legend("FFT","apFFT"); ylabel("Amplitude");
+    hold off;
+    ax(2) = subplot(3,1,2);
+    plot(f_FFT(1:end-1),angle(S_FFT(k)),'LineWidth',1)
+    hold on
+    plot(f_FFT(1:end-1),angle(S_apFFT(k)),'LineWidth',1)
+    legend("FFT","apFFT"); ylabel("Phase");
+    hold off;
+    ax(3) = subplot(3,1,3);
+    plot(f_correct,abs(A),'LineWidth',1)
+    legend("Corrected apFFT"); ylabel("Amplitude");
+    linkaxes(ax,'x')
 end
-
-
-% Step 2: Generate the All Phase sequence
-% For demonstration, this example assumes a simple phase adjustment.
-% In practice, this step might involve more complex phase manipulation
-% depending on the specific requirements of the all-phase transformation.
-n = length(signal);
-phaseFactor = exp(-1i * 2 * pi * (0:n-1) / n);
-allPhaseFFT = fftSignal .* phaseFactor;
 
 end

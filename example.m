3110d208bb6871e0d2a20e3bdaad0238c7620ca6
ThalerG <thalergabriel@gmail.com>
2024-07-15 14:42:04 -0300

    Add file for group double window instead of convolution

--- example.m
+++ example.m
@@ -3,23 +3,28 @@ clear; close all;
 %% Signal:
 
 Fs = 1024; % Sampling freq
-N = 4096; % Number of samples
+N = 2048; % Number of samples
 
 f = [50.1,75.3,100.4,125.2,150.3]; % Frequency of signal components (Hz)
 A = [220,0.05,0.1,25,7]; % Amplitude of signal components
-ph = [40,20,30,70,60]; % Phase of signal components (deg)
+ph = deg2rad([40,20,30,70,60]); % Phase of signal components (rad)
+
+% f = [50,80]; % Frequency of signal components (Hz)
+% A = [4,2]; % Amplitude of signal components
+% ph = [0,-pi/4]; % Phase of signal components (rad)
 
 t=(0:N-1)/Fs;
 x = zeros(1,length(t));
 
 for k = 1:length(f)
-    x = x + A(k)*cos(2*pi*f(k)*t+ph(k)*pi/180);
+    x = x + A(k)*cos(2*pi*f(k)*t+ph(k));
 end
 
 %% Corrected double-window apFFT:
 
 % Corrected apFFT
 [f_apFFT,y_apFFT] = Nuttall_2win_apFFT(x(1:end-1),Fs);
+% [f_apFFT2,y_apFFT2] = doublewin_apFFT_group(x(1:end-1),Fs);
 
 % Traditional FFT
 f_FFT = Fs/N*(0:(N/2));
